5a9b4826f8c7d9e0e340a5a9db632c8b1db6e896
Deyler Mena <deyler_mena69222@elpoli.edu.co>
2024-10-05 10:22:31 -0500

    cmabio de DB

--- src/main/java/com/example/util/DatabaseConnection.java
+++ src/main/java/com/example/util/DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/systemposjava";
-    private static final String USER = "andres";
-    private static final String PASSWORD = "R@@t12345";
+    private static final String URL = "jdbc:mysql://autorack.proxy.rlwy.net:23483/railway";
+    private static final String USER = "root";
+    private static final String PASSWORD = "oRlHogGxCBlqVlldWshcTTyGGidYStVx";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
